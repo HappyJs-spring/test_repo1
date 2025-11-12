74a287a802a7332631f53d217b49fe5410a000f8
doseong :) <118369702+HappyJs-spring@users.noreply.github.com>
2025-11-12 16:00:26 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
-#include <stdio.h>
+    #include <stdio.h>
+    #include "hangman.h"
 
-
-int main(void) {
-
-  return 0;
-}
+    int main(void) {
+        printf("=== 게임 실행 ===\n\n");
+        playHangman(); // 언제든 호출 가능
+        printf("\n=== 게임 종료 ===\n");
+        return 0;
+    }
+    
